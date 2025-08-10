343a2eef2202b9e70dc4c33d279c4ff5953f5d8a
William de Kock <amor.cdk@gmail.com>
2025-08-10 06:58:01 +0200

    removed comments

--- src/main/java/net/williamcooke/SkyOrigns2Tech/SkyOrigns2Tech.java
+++ src/main/java/net/williamcooke/SkyOrigns2Tech/SkyOrigns2Tech.java
@@ -13,13 +13,13 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
+
 @Mod(SkyOrigns2Tech.Mod_Id)
 public class SkyOrigns2Tech
 {
-    // Define mod id in a common place for everything to reference
+
     public static final String Mod_Id = "sky_origins_tech_2";
-    // Directly reference a slf4j logger
+
     private static final Logger LOGGER = LogUtils.getLogger();
 
 
