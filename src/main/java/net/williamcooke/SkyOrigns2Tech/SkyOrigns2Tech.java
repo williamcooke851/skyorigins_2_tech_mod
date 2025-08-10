0229a4217d283226da3a60d9f5d6a76520916687
William de Kock <amor.cdk@gmail.com>
2025-08-10 12:47:36 +0200

    added items

--- src/main/java/net/williamcooke/SkyOrigns2Tech/SkyOrigns2Tech.java
+++ src/main/java/net/williamcooke/SkyOrigns2Tech/SkyOrigns2Tech.java
@@ -1,6 +1,8 @@
 package net.williamcooke.SkyOrigns2Tech;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -11,6 +13,8 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.williamcooke.SkyOrigns2Tech.block.ModBlocks;
+import net.williamcooke.SkyOrigns2Tech.item.ModItems;
 import org.slf4j.Logger;
 
 
@@ -26,6 +30,10 @@ public class SkyOrigns2Tech
     public SkyOrigns2Tech(FMLJavaModLoadingContext context) {
         IEventBus modEventBus = context.getModEventBus();
 
+
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
+
         modEventBus.addListener(this::commonSetup);
 
         MinecraftForge.EVENT_BUS.register(this);
@@ -38,6 +46,15 @@ public class SkyOrigns2Tech
 
 
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
+         if (event.getTabKey() == CreativeModeTabs.INGREDIENTS){
+             event.accept(ModItems.voidEssence);
+         }
+         if (event.getTabKey() == CreativeModeTabs.INGREDIENTS){
+             event.accept(ModItems.voidCrystal);
+         }
+         if (event.getTabKey()== CreativeModeTabs.BUILDING_BLOCKS){
+             event.accept(ModBlocks.Block_of_voidcrystals);
+         }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/williamcooke/SkyOrigns2Tech/item/ModItems.java
@@ -0,0 +1,23 @@
+package net.williamcooke.SkyOrigns2Tech.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.williamcooke.SkyOrigns2Tech.SkyOrigns2Tech;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, SkyOrigns2Tech.Mod_Id);
+
+
+    public static final RegistryObject<Item> voidEssence = ITEMS.register("voidessence",
+            ()-> new Item(new Item.Properties()));
+public static final RegistryObject<Item> voidCrystal = ITEMS.register("voidcrystal",
+        ()->new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+}
